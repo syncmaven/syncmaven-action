3e922ee209bfd4ed9b56813d50eb07be60924b18
Vladimir Klimontovich <vladimir@jitsu.com>
2024-11-18 10:14:30 -0500

    Added options introduced in Syncmaven lately, mainly to run jobs without project dir by specifying sources and destinations right in the command line

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# syncmaven-action
+# Syncmaven GitHub Action
+
+This repo contains source code for the Syncmaven GitHub Action. Read more:
+ * About [Syncmaven](https://syncmaven.com)
+ * About scheduling Syncmaven jobs with [GitHub Actions](https://syncmaven.sh/quickstart/production#scheduled-sync-with-github-actions)
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,93 @@
 #!/usr/bin/env bash
 
-awk 'BEGIN{for(v in ENVIRON) print v}' > .env
+# Function to escape special characters - line breaks and quotes, so the value
+# can be inlined in the command line
+escape() {
+  local input="$*"
+  printf '%q' "$input"
+}
 
-SYNC_ARGS=""
+#unlike printenv, this function will print multi-line values correctly
+function print_env_vars {
+  for var in $(compgen -e); do
+    printf '%s=%q\n' "$var" "${!var}"
+  done
+}
 
-REPOSITORY=$(echo "${RUNNER_WORKSPACE##*/}")
+main() {
+  # Save all environment variables to .env file
+  awk 'BEGIN{for(v in ENVIRON) print v}' > .env
 
-PROJECT_DIR="$RUNNER_WORKSPACE/$REPOSITORY"
+  SYNC_ARGS=()
 
-if [ ! -z $INPUT_DIR ]; then
-  PROJECT_DIR="$PROJECT_DIR/$INPUT_DIR"
-fi
+  DEBUG_MODE=false
+  if [ -n "$INPUT_DEBUG" ]; then
+    SYNC_ARGS+=("--debug")
+    echo "Input variables:"
+    print_env_vars
+    DEBUG_MODE=true
+  fi
 
-if [ ! -z $INPUT_SELECT ]; then
-  SYNC_ARGS="$SYNC_ARGS --select $INPUT_SELECT"
-fi
+  REPOSITORY=$(basename "$RUNNER_WORKSPACE")
 
-if [ ! -z $INPUT_STATE ]; then
-  SYNC_ARGS="$SYNC_ARGS --state $INPUT_STATE"
-fi
 
-if [ ! -z $INPUT_DEBUG ]; then
-  SYNC_ARGS="$SYNC_ARGS --debug"
-fi
+  PROJECT_DIR="$RUNNER_WORKSPACE/$REPOSITORY"
 
-if [ ! -z $INPUT_FULL ]; then
-  SYNC_ARGS="$SYNC_ARGS --full-refresh"
-fi
+  if [ -n "$INPUT_DIR" ]; then
+    PROJECT_DIR="$PROJECT_DIR/$INPUT_DIR"
+  fi
 
-export RPC_PORT=8081
+  # Mapping of INPUT variables to command-line options
+  declare -A ARG_MAPPING=(
+    ["INPUT_SELECT"]="--select"
+    ["INPUT_STATE"]="--state"
+    ["INPUT_FULL"]="--full-refresh"
+    ["INPUT_MODEL"]="--model"
+    ["INPUT_DATASOURCE"]="--datasource"
+    ["INPUT_CREDENTIALS"]="--credentials"
+    ["INPUT_SYNC-ID"]="--sync-id"
+    ["INPUT_PACKAGE"]="--package"
+    ["INPUT_PACKAGE-TYPE"]="--package-type"
 
-docker run -e RPC_PORT --env-file .env -p 8081:8081 -v $PROJECT_DIR:/project -v /var/run/docker.sock:/var/run/docker.sock syncmaven/syncmaven:latest sync $SYNC_ARGS
+  )
+
+  for VAR in "${!ARG_MAPPING[@]}"; do
+    VALUE="${!VAR}"
+    if [ -n "$VALUE" ]; then
+      ARG="${ARG_MAPPING[$VAR]}"
+      VALUE="$(escape "$VALUE")"
+      # For flags without values (e.g., --full-refresh), don't add the value
+      if [ "$ARG" == "--full-refresh" ]; then
+        SYNC_ARGS+=("$ARG")
+      else
+        SYNC_ARGS+=("$ARG" "$VALUE")
+      fi
+    fi
+  done
+
+  SYNCMAVEN_VERSION="${INPUT_SYNCMAVEN-VERSION:-latest}"
+
+  export RPC_PORT=8081
+
+  # Build the docker command as an array
+  DOCKER_CMD=(
+    docker run
+    -e RPC_PORT
+    --env-file .env
+    -p 8081:8081
+    -v "$PROJECT_DIR:/project"
+    -v /var/run/docker.sock:/var/run/docker.sock
+    "syncmaven/syncmaven:${SYNCMAVEN_VERSION}"
+    sync
+    "${SYNC_ARGS[@]}"
+  )
+
+  if [ "$DEBUG_MODE" = true ]; then
+    echo "Docker command:" "${DOCKER_CMD[@]}"
+  fi
+
+  # Execute the docker command
+  "${DOCKER_CMD[@]}"
+}
+
+main "$@"
